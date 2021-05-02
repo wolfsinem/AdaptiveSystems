d21994bf16ca3315d5db64b516e1fe2bd52bce04
wolfsinem <sinem.ertem@student.hu.nl>
2021-05-03 01:57:25 +0200

    add first visit mc

--- evaluation/monte_carlo.py
+++ evaluation/monte_carlo.py
@@ -36,7 +36,7 @@ def mc_evaluation_policy(env, discount_factor):
     maze_rewards[13] = -2
     maze_rewards[6] = -10
     maze_rewards[7] = -10
-    
+
     for ep in range(MAX_EP):
         G = 0
         start_state = env.reset()
@@ -56,6 +56,94 @@ def mc_evaluation_policy(env, discount_factor):
                 maze_rewards[step[0]] = round(
                     np.mean(rewards[str(step[0])]), 2)
 
+    return maze_rewards
+
+
+def probability(A):
+    """[summary]
+
+    Args:
+        A ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    epsilon = 0.25
+    idx = np.argmax(A)
+    probs = []
+    A_ = np.sqrt(sum([i**2 for i in A]))
+
+    if A_ == 0:
+        A_ = 1.0
+    for i, a in enumerate(A):
+        if i == idx:
+            probs.append(round(1-epsilon + (epsilon/A_), 3))
+        else:
+            probs.append(round(epsilon/A_, 3))
+    err = sum(probs)-1
+    substracter = err/len(A)
+
+    return np.array(probs)-substracter
+
+
+def rewards_lists(cells, actions):
+    """Initialize rewards lists
+
+    Args:
+        cells ([type]): [description]
+        actions ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    # Each state has four possible actions to take
+    def create_array(n, lst):
+        for i in range(n):
+            lst.append(str(i))
+        return lst
+
+    possible_states = []
+    possible_states = create_array(cells, possible_states)
+
+    possible_actions = []
+    possible_actions = create_array(actions, possible_actions)
+
+    rewards = {}
+    for state in possible_states:
+        for action in possible_actions:
+            rewards[state+", "+action] = []
+
+    return rewards
+
+def first_visit_mc(env, max_ep, discount_factor, rewards):
+
+    maze_rewards = np.zeros((16, 4))
+    maze_rewards[3] = 40
+    maze_rewards[12] = 10
+    maze_rewards[13] = -2
+    maze_rewards[6] = -10
+    maze_rewards[7] = -10
+
+    for ep in range(max_ep):
+        G = 0
+        state = env.reset()
+        trajectory = []
+        while True:
+            action_values = maze_rewards[state]
+            probs = probability(action_values)
+            action = np.random.choice(np.arange(4), p=probs)
+            next_state, reward, done = env.step(action)
+            trajectory.append((state, action, reward))
+            state = next_state
+            if done:
+                break
+        for idx, step in enumerate(trajectory[::-1]):
+            G = discount_factor * G + step[2]
+            # first visit check
+            if step[0] not in np.array(trajectory[::-1])[:, 0][idx+1:]:
+                rewards[str(step[0])+", "+str(step[1])].append(G)
+                maze_rewards[step[0]][step[1]] = np.mean(
+                    rewards[str(step[0])+", "+str(step[1])])
     return maze_rewards
 
 
