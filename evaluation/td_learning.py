bf965e46a7901c9303ffb3e02553647116a1c32d
wolfsinem <sinem.ertem@student.hu.nl>
2021-05-21 02:08:27 +0200

    rename some terms (values/returns/rewards)

--- evaluation/td_learning.py
+++ evaluation/td_learning.py
@@ -1,29 +1,31 @@
 import numpy as np
 import random
 
-from maze_class import *
-from coords import *
+from maze_class import Maze
+from coords import maze_coords, reversed_maze
 
 # Initialize parameters
 MAX_EP = 5000
 ALPHA = 0.01  # learning rate
 ACTIONS = [0, 1, 2, 3]
 
-# Initialize maze_rewards with 16 cells (one value per cell)
-maze_rewards = np.zeros(16)
-maze_rewards[3] = 40
-maze_rewards[12] = 10
-maze_rewards[13] = -2
-maze_rewards[6] = -10
-maze_rewards[7] = -10
+# Initialize state value grid with 16 cells (one value per cell)
+# For TD learning
+state_val_grid = np.zeros(16)
+state_val_grid[3] = 40
+state_val_grid[12] = 10
+state_val_grid[13] = -2
+state_val_grid[6] = -10
+state_val_grid[7] = -10
 
-# Initialize new maze_rewards which has 4 probabilities per cell (16x4)
-q_maze_rewards = np.zeros((16, 4))
-q_maze_rewards[3] = 40
-q_maze_rewards[12] = 10
-q_maze_rewards[13] = -2
-q_maze_rewards[6] = -10
-q_maze_rewards[7] = -10
+# Initialize new state value grid which has 4 probabilities per cell (16x4)
+# For Q-Learning
+state_val_grid_ = np.zeros((16, 4))
+state_val_grid_[3] = 40
+state_val_grid_[12] = 10
+state_val_grid_[13] = -2
+state_val_grid_[6] = -10
+state_val_grid_[7] = -10
 
 def td_learning(env, discount_factor):
     """Temporal difference (TD) learning refers to a class of model-free
@@ -40,18 +42,18 @@ def td_learning(env, discount_factor):
         G = 0
         state = env.reset()
         while True:
-            action = random.choice(ACTIONS)  # random policy
+            action = random.choice(ACTIONS)  # Choose random policy
             next_state, reward, done = env.step(action)
 
             # Updating
-            maze_rewards[state] += ALPHA * \
+            state_val_grid[state] += ALPHA * \
                 (reward + discount_factor *
-                 maze_rewards[next_state] - maze_rewards[state])
+                 state_val_grid[next_state] - state_val_grid[state])
             state = next_state
             if done:
                 break
 
-    return maze_rewards
+    return state_val_grid
 
 
 def get_action(q_values, epsilon):
@@ -86,20 +88,20 @@ def sarsa(env, discount_factor, alpha):
     for ep in range(MAX_EP):
         iterator = 1
         state = env.reset()
-        action = get_action(q_maze_rewards[state], 1)
+        action = get_action(state_val_grid_[state], 1)
         while True:
             next_state, reward, done = env.step(action)
             # Updating
             iterator += 1
             epsilon = 1 * 0.99 ** iterator
-            next_action = get_action(q_maze_rewards[next_state], epsilon)
-            q_maze_rewards[state][action] += alpha*(reward + discount_factor *
-                                                    q_maze_rewards[next_state][next_action] - q_maze_rewards[state][action])
+            next_action = get_action(state_val_grid_[next_state], epsilon)
+            state_val_grid_[state][action] += alpha*(reward + discount_factor *
+                                                    state_val_grid_[next_state][next_action] - state_val_grid_[state][action])
             state = next_state
             action = next_action
             if done:
                 break
-    return q_maze_rewards
+    return state_val_grid_
 
 
 def sarsaMAX(env, discount_factor, alpha):
@@ -119,16 +121,16 @@ def sarsaMAX(env, discount_factor, alpha):
         iterator = 1
         state = env.reset()
         while True:
-            action = get_action(q_maze_rewards[state], 1 * 0.95**iterator)
+            action = get_action(state_val_grid_[state], 1 * 0.95**iterator)
             next_state, reward, done = env.step(action)
 
             # Updating
-            q_maze_rewards[state][action] += alpha * (reward + discount_factor * max(
-                q_maze_rewards[next_state]) - q_maze_rewards[state][action])
+            state_val_grid_[state][action] += alpha * (reward + discount_factor * max(
+                state_val_grid_[next_state]) - state_val_grid_[state][action])
 
             iterator += 1
             state = next_state
 
             if done:
                 break
-    return q_maze_rewards
+    return state_val_grid_
